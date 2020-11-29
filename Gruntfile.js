fe07c7e7024f769997f818dbd82d5220e40e586f
Albi Taulla <artemis@DESKTOP-NM3F64C.localdomain>
2020-11-29 22:13:48 +0100

    Grunt Part Two

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,9 @@ module.exports = function(grunt){
 
     //make sure every glunt plugin is automatically loaded when used
     //could also be done manually
-    require('jit-grunt')(grunt);
+    require('jit-grunt')(grunt, {
+        useminPrepare: 'grunt-usemin'
+    });
     
     grunt.initConfig({
         sass: {
@@ -42,6 +44,122 @@ module.exports = function(grunt){
                     }
                 }
             }
+        },
+        copy: {
+            html: {
+                files: [{
+                    expand: true,
+                    dot: true,
+                    cwd: './',
+                    src: ['*.html'],
+                    dest: 'dist'
+                }]
+            },
+            fonts: {
+                files: [{
+                    expand: true,
+                    dot: true,
+                    cwd: 'node_modules/font-awesome',
+                    src: ['fonts/*.*'],
+                    dest: 'dist'
+                }]
+            }
+        },
+        clean:{
+            build:{
+                src: ['dist/']
+            }
+        },
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true,
+                    dot: true,
+                    cwd: './',
+                    src: ['img/*.{png,jpg,gig}'],
+                    dest: 'dist/'
+                }]
+            }
+        },
+        useminPrepare: {
+            foo: {
+                dest: 'dist',
+                src: ['contactus.html','aboutus.html','index.html']
+            },
+            options:{
+                flow: {
+                    steps: {
+                        css: ['cssmin'],
+                        js: ['uglify']
+                    },
+                    post: {
+                        css: [{
+                            name: 'cssmin',
+                            createConfig: function(context, block){
+                                var generated = context.options.generated;
+                                generated.options = {
+                                    keepSpecialComments: 0,rebase: false
+                                };
+                            }
+                        }]
+                    }
+                }
+            }
+        },
+        concat: {
+            options: {
+                separator: ';'
+            },
+            dist: {
+
+            }
+        },
+        uglify: {
+            dist: {}
+        },
+        cssmin: {
+            dist:{}
+        },
+        //If someone has visited your site previously,the browser may keep
+        //the main.js and main.css file in their local cache and if you
+        //update those , when someone revisits your website,
+        //the browser does not download them because it has them on the cache
+        //Adds additional extendsion to the main name
+        //Add a file revision number every time we prepare a dist folder
+        //It takes the contents,processed and produces an md5 20 character
+        //number attached to the main file.
+        filerev: {
+            options: {
+                encoding: 'utf8',
+                algorithm: 'md5',
+                length: 20
+            },
+            release: {
+                files: [{
+                    src: [
+                        'dist/js/*.js',
+                        'dist/css/*.css'
+                    ]
+                }]
+            },
+        },
+        usemin: {
+            html: ['dist/contactus.html','dist/aboutus.html','dist/index.html'],
+            options: {
+                assetsDirs: ['dist','dist/css','dist/js']
+            }
+        },
+        htmlmin: {
+            dist: {
+                options: {
+                    collapseWhiteSpace: true
+                },
+                files: {
+                    'dist/index.html' : 'dist/index.html',
+                    'dist/contactus.html' : 'dist/contactus.html',
+                    'dist/aboutus.html' : 'dist/aboutus.html'
+                }
+            }
         }
     });
     //Configure the sass task
@@ -49,4 +167,16 @@ module.exports = function(grunt){
     grunt.registerTask('css',['sass']);
     //The watch task stops all the other tasks after so it must be done last
     grunt.registerTask('default',['browserSync','watch']);
+    grunt.registerTask('build',[
+        'clean',
+        'copy',
+        'imagemin',
+        'useminPrepare',
+        'concat',
+        'cssmin',
+        'uglify',
+        'filerev',
+        'usemin',
+        'htmlmin'
+    ])
 };
